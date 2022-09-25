b118824f40fdcf9c1100e150f6885809990823b5
samuel santos <samaraujo28@gmail.com>
2022-09-25 02:02:01 -0300

    feat: implement create a room in database logic

---
+++ lib/suneater/room/create.ex
@@ -0,0 +1,12 @@
+defmodule Suneater.Room.Create do
+  alias Suneater.{Repo, Room}
+
+  def call(params) do
+    params
+    |> Room.build()
+    |> create_room()
+  end
+
+  defp create_room({:ok, struct}), do: Repo.insert(struct)
+  defp create_room({:error, _changeset} = error), do: error
+end
