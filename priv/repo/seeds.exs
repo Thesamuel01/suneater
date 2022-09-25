f0ed8efa7e1200463bbbff652ee80bf101e4b34e
samuel santos <samaraujo28@gmail.com>
2022-09-25 01:58:49 -0300

    chore: implement seeds script for development stage

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Suneater.{Repo, Room}
+
+Repo.insert!(%Room{
+  id: "c3e848c1-6e4f-4bc7-b9ee-9af73262c1f3",
+  name: "Cinema",
+  description: "Random Description",
+  topic: "entertainment"
+})
+
+Repo.insert!(%Room{
+  id: "54a88942-7706-4f98-9a31-c5523affce72",
+  name: "Sports",
+  description: "Random Description",
+  topic: "sport"
+})
+
+Repo.insert!(%Room{
+  id: "f18ec984-410d-473c-bddf-3d130280275e",
+  name: "Conversation",
+  description: "Random Description",
+  topic: "any"
+})
