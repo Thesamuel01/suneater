cbec7ddb238d1aa37ee9141872b83a1662c8cd10
samuel santos <samaraujo28@gmail.com>
2022-09-26 01:35:50 -0300

    refact: delegate get general rooms

--- lib/suneater.ex
+++ lib/suneater.ex
@@ -2,5 +2,6 @@ defmodule Suneater do
   alias Suneater.Room
 
   defdelegate get_all_rooms(), to: Room.GetAll, as: :call
+  defdelegate get_all_general_rooms(), to: Room.General.GetAll, as: :call
   defdelegate create_room(params), to: Room.Create, as: :call
 end
