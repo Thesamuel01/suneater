15fbeb80ea13a8009aad0e8d375dcdfe27ff4f3c
samuel santos <samaraujo28@gmail.com>
2022-09-26 01:30:44 -0300

    feat: update migrations, insert a new column in rooms table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -16,19 +16,22 @@ Repo.insert!(%Room{
   id: "c3e848c1-6e4f-4bc7-b9ee-9af73262c1f3",
   name: "Cinema",
   description: "Random Description",
-  topic: "entertainment"
+  topic: "entertainment",
+  type: "general"
 })
 
 Repo.insert!(%Room{
   id: "54a88942-7706-4f98-9a31-c5523affce72",
   name: "Sports",
   description: "Random Description",
-  topic: "sport"
+  topic: "sport",
+  type: "general"
 })
 
 Repo.insert!(%Room{
   id: "f18ec984-410d-473c-bddf-3d130280275e",
   name: "Conversation",
   description: "Random Description",
-  topic: "any"
+  topic: "any",
+  type: "general"
 })
